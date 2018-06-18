aaaa9a0790e59d878234eb6fe4aca2955e2bddbd
Ben <wongben16@gmail.com>
2018-06-18 10:35:19 -0400

    BigInteger

--- Factorial 5000/Factorial 5000/Program.cs
+++ Factorial 5000/Factorial 5000/Program.cs
@@ -4,9 +4,26 @@ namespace Factorial_5000
 {
     class Program
     {
+        public static int factor;  
+
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please input a number from 1 to 5000.");
+            do
+            {
+                if (factor < 1 || factor > 5000)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                factor = Int32.Parse(Console.ReadLine());
+            } while (factor < 1 || factor > 5000);
+
+            if (factor > 100)
+            {
+                System.Numerics.BigInteger bigFactor = new System.Numerics.BigInteger(factor);
+            }
+
+
         }
     }
 }
