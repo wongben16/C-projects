9fb2d947e5f36723ba0710fef2a2877955957141
Ben <wongben16@gmail.com>
2018-06-20 14:47:50 -0400

    for loop not working

--- TelephoneNum/TelephoneNum/Program.cs
+++ TelephoneNum/TelephoneNum/Program.cs
@@ -6,12 +6,38 @@ namespace TelephoneNum
     class Program
     {
         public static String[] telephoneNums;
+        public static StreamReader nums = new StreamReader(@"C:\Users\Ben\C-projects\TelephoneNum\TelephoneNumberingPlan.txt");
+        public static String temp;
+        public static String temp2;
+        public static int counter = 0;
 
         static void Main(string[] args)
         {
+            //Setup();
+
+            ////test for textfile opening
+            //while ((temp = nums.ReadLine()) != null)
+            //{
+            //    System.Console.WriteLine(temp);
+            //    counter++;
+            //}
 
 
-            Setup();
+
+
+            while ((temp = nums.ReadLine()) != null)
+            {
+                for (int i = 0; (temp = nums.ReadLine()) != null; i++)
+                {
+                    temp.Split(@"\s+");
+                    telephoneNums[i] = temp;
+                }
+            }
+
+            //for (int i = 0; i > telephoneNums.Length; i++)
+            //{
+            //    System.Console.WriteLine(telephoneNums[i]);
+            //}
 
             System.Console.WriteLine("Please enter your phone number.");
             System.Console.ReadLine();
@@ -21,19 +47,17 @@ namespace TelephoneNum
             System.Console.ReadKey();
         }
 
-        public static void Setup()
-        {
-            StringReader nums = new StringReader("Telephone Numbering Plan");
-            String temp;
-            String temp2;
+        //public static void Setup()
+        //{
 
-            while ((temp = nums.ReadLine()) != null)
-            {
-                for (int i = 0; (temp = nums.ReadLine()) != null; i++) {
-                    temp.Split(@"\s+");
-                    telephoneNums[i] = temp;
-                }
-            }
-        }
+
+        //    while ((temp = nums.ReadLine()) != null)
+        //    {
+        //        for (int i = 0; (temp = nums.ReadLine()) != null; i++) {
+        //            temp.Split(@"\s+");
+        //            telephoneNums[i] = temp;
+        //        }
+        //    }
+        //}
     }
 }
