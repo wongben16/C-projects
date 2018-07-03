1c00d27685e388c97e7bb823478bca2393de02be
Ben <wongben16@gmail.com>
2018-07-03 10:18:21 -0400

    wrote scanner class

--- TelephoneNum/TelephoneNum/Program.cs
+++ TelephoneNum/TelephoneNum/Program.cs
@@ -44,4 +44,71 @@ namespace TelephoneNum
         //    }
         //}
     }
+
+    class Scanner : System.IO.StringReader
+    {
+        String currentWord;
+
+        public Scanner(String source) : base(source)
+        {
+            readNextWord();
+        }
+
+        private void readNextWord()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            char nextChar;
+            int next;
+            do
+            {
+                next = this.Read();
+                if (next < 0)
+                {
+                    break;
+                }
+                nextChar = (char)next;
+                if (char.IsWhiteSpace(nextChar))
+                {
+                    break;
+                }
+                sb.Append(nextChar);
+            } while (true);
+            while ((this.Peek() >= 0) && (char.IsWhiteSpace((char)this.Peek())))
+            {
+                this.Read();
+            }
+            if (sb.Length > 0)
+            {
+                currentWord = sb.ToString();
+            }
+            else
+            {
+                currentWord = null;
+            }
+        }
+
+        public bool hasNextInt()
+        {
+            if (currentWord == null)
+            {
+                return false;
+            }
+
+            int dummy;
+            return int.TryParse(currentWord, out dummy);
+        }
+
+        public int nextInt()
+        {
+            try
+            {
+                return int.Parse(currentWord);
+            }
+            finally
+            {
+                readNextWord();
+            }
+        }
+        
+    }
 }
